7c5537707a64a8920d58291f0b1d566d54705a38
Jeff A <jeff@umd.edu>
2017-02-18 02:03:42 -0500

    initial push to Github from Win10
    transfer from Win10 to Mac

---
+++ Lesson1-Feb3-SpaceyGame/Assets/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public Rigidbody2D bulletPreFab;
+    public float bulletSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float xInput = Input.GetAxis ("Horizontal");
+		float yInput = Input.GetAxis ("Vertical");
+
+		Vector2 mousePos = Input.mousePosition;
+		Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint (mousePos);
+
+		Vector3 input = new Vector3 (xInput, yInput, 0);
+		transform.position += input * 2 * Time.deltaTime;
+	
+		Debug.Log (worldMousePosition);
+		//transform.LookAt (worldMousePosition);
+
+		// get direction you want to point at
+		Vector2 direction = (worldMousePosition - (Vector2) transform.position).normalized;
+
+		// set vector of transform directly
+		transform.up = direction;
+
+
+        // fire
+        if(Input.GetMouseButtonDown(0))
+        {
+            //create bullet
+            Rigidbody2D instantiatedBullet = Instantiate(bulletPreFab, transform.position, transform.rotation) as Rigidbody2D;
+            //instantiatedBullet.velocity = new Vector2(10, 0);
+            instantiatedBullet.velocity = transform.up * bulletSpeed;
+            Destroy(instantiatedBullet.gameObject, 1);
+        } 
+	}
+}
